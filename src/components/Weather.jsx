eb9254d73bea984a5f1701462057b10a5dacef4d
muskan170604 <muskan_ug_23@civil.nits.ac.in>
2025-05-30 01:20:55 +0530

    adding assets

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -3,8 +3,11 @@ import './Weather.css'
 
 const Weather = () => {
   return (
-    <div>
-        
+    <div className='weather'>
+        <div className='search-bar'>
+            <input type="text" placeholder='Search' />
+            <img src="" alt="" />
+        </div>
     </div>
   )
 }
